1bf5b22792efa99215303a5c076cf8b07f62a3a8
Alex Bance <alex.bance@live.co.uk>
2022-11-08 20:01:38 +0000

    fix: switched to default-jre

--- terraform/components/gameserver/bootstrap.sh
+++ terraform/components/gameserver/bootstrap.sh
@@ -4,7 +4,7 @@
 apt update
 apt upgrade
 
-apt install -y openjdk-19-jre perl libjson-perl libwww-perl liblwp-protocol-https-perl util-linux python make wget git rdiff-backup rsync socat iptables
+apt install -y default-jre perl libjson-perl libwww-perl liblwp-protocol-https-perl util-linux python make wget git rdiff-backup rsync socat iptables
 
 git clone https://github.com/MinecraftServerControl/mscs.git && cd mscs
 make install
