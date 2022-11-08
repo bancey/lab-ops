6ffa3ba403fa06c0a2efc063597fdddb6f1d2ee8
Alex Bance <abance@bancey.xyz>
2022-11-08 11:50:00 +0000

    feat: added nsg

--- terraform/components/gameserver/40-networking.tf
+++ terraform/components/gameserver/40-networking.tf
@@ -23,6 +23,27 @@ resource "azurerm_subnet" "this" {
   address_prefixes     = var.gameserver_vnet_address_space
 }
 
+resource "azurerm_network_security_group" "this" {
+  name                = "${var.gameserver_name}-${var.env}-nsg"
+  resource_group_name = local.resource_group_name
+  location            = local.resource_group_location
+  tags                = local.tags
+}
+
+resource "azurerm_network_security_rule" "Allow_SSH" {
+  network_security_group_name = azurerm_network_security_group.this.name
+  resource_group_name         = local.resource_group_name
+  name                        = "AllowSSH"
+  priority                    = 1000
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  destination_address_prefix  = "*"
+  source_address_prefix       = var.admin_access_ip
+}
+
 resource "azurerm_network_interface" "this" {
   name                = "${var.gameserver_name}-${var.env}-nic"
   location            = local.resource_group_location
