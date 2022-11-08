46ed52067d5d149fdff984e6ad951e3a115ff994
Alex Bance <abance@bancey.xyz>
2022-11-08 17:22:22 +0000

    feat: added bootstrap script

---
+++ terraform/components/gameserver/60-extensions.tf
@@ -0,0 +1,13 @@
+resource "azurerm_virtual_machine_extension" "customscript" {
+  name                       = "BootstrapVM"
+  virtual_machine_id         = azurerm_linux_virtual_machine.this.id
+  publisher                  = "Microsoft.Azure.Extensions"
+  type                       = "CustomScript"
+  type_handler_version       = 2.1
+  auto_upgrade_minor_version = false
+  protected_settings         = <<PROTECTED_SETTINGS
+  {
+    "script": base64encode(file("${path.module}/bootstrap.sh")),
+  }
+  PROTECTED_SETTINGS
+}
---
+++ terraform/components/gameserver/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#set -vxn
+
+apt update
+apt upgrade
+
+apt install -y openjdk-19-jre perl libjson-perl libwww-perl liblwp-protocol-https-perl util-linux python make wget git rdiff-backup rsync socat iptables
+
+git clone https://github.com/MinecraftServerControl/mscs.git && cd mscs
+make install
+
+
