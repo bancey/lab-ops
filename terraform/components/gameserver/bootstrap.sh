204b43d1bdc01894703f9217d1b01c3be5b5aa3a
Alex Bance <alex.bance@live.co.uk>
2022-11-08 20:14:31 +0000

    fix: updated bootstrap script

--- terraform/components/gameserver/bootstrap.sh
+++ terraform/components/gameserver/bootstrap.sh
@@ -2,9 +2,9 @@
 #set -vxn
 
 apt update
-apt upgrade
+apt upgrade -y
 
-apt install -y default-jre perl libjson-perl libwww-perl liblwp-protocol-https-perl util-linux python make wget git rdiff-backup rsync socat iptables
+apt install -y default-jre perl libjson-perl libwww-perl liblwp-protocol-https-perl util-linux python3 make wget git rdiff-backup rsync socat iptables
 
 git clone https://github.com/MinecraftServerControl/mscs.git && cd mscs
 make install
