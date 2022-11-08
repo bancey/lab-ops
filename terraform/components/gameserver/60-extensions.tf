372095ca12d2220ca03877d51e146957d7a26bbd
Alex Bance <abance@bancey.xyz>
2022-11-08 17:26:26 +0000

    fix: syntax error

--- terraform/components/gameserver/60-extensions.tf
+++ terraform/components/gameserver/60-extensions.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_machine_extension" "customscript" {
   auto_upgrade_minor_version = false
   protected_settings         = <<PROTECTED_SETTINGS
   {
-    "script": base64encode(file("${path.module}/bootstrap.sh")),
+    "script": "${base64encode(file("${path.module}/bootstrap.sh"))}",
   }
   PROTECTED_SETTINGS
 }
