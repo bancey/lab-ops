6501d1617404c7d93fce368c7ee0d954710faac7
Alex Bance <alex.bance@live.co.uk>
2022-12-12 22:21:40 +0000

    fix: restart -> reboot

--- terraform/modules/proxmox-vm/vm.tf
+++ terraform/modules/proxmox-vm/vm.tf
@@ -1,19 +1,19 @@
 resource "proxmox_vm_qemu" "vm" {
-  target_node      = var.target_node
-  vmid             = var.vm_id
-  name             = var.vm_name
-  desc             = var.vm_description
-  onboot           = var.start_on_boot
-  startup          = "order=${var.startup_order},up=${var.startup_delay}"
-  clone            = "ubuntu-jammy-template"
-  agent            = var.qemu_agent_installed
-  cores            = var.cpu_cores
-  sockets          = var.cpu_sockets
-  cpu              = "host"
-  memory           = var.memory
-  os_type          = "cloud-init"
-  ipconfig0        = "ip=${var.ip_address}/32,gw=192.168.80.1"
-  restart_required = true
+  target_node     = var.target_node
+  vmid            = var.vm_id
+  name            = var.vm_name
+  desc            = var.vm_description
+  onboot          = var.start_on_boot
+  startup         = "order=${var.startup_order},up=${var.startup_delay}"
+  clone           = "ubuntu-jammy-template"
+  agent           = var.qemu_agent_installed
+  cores           = var.cpu_cores
+  sockets         = var.cpu_sockets
+  cpu             = "host"
+  memory          = var.memory
+  os_type         = "cloud-init"
+  ipconfig0       = "ip=${var.ip_address}/32,gw=192.168.80.1"
+  reboot_required = true
 
   network {
     bridge = "vmbr0"
