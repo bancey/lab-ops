1763dbe464f67b86d2df61bc084c6f03b81bd217
Alex Bance <alex.bance@live.co.uk>
2022-12-13 10:11:53 +0000

    feat: expanded to allow multiple vms easily

---
+++ terraform/components/k8s/inputs-optional.tf
@@ -0,0 +1,23 @@
+variable "master_cidr" {
+  type        = string
+  description = "The IPv4 address of the master node."
+  default     = "192.168.80.64/28"
+}
+
+variable "master_count" {
+  type        = number
+  description = "The number of master nodes to create."
+  default     = 1
+}
+
+variable "nodes_cidr" {
+  type        = string
+  description = "The IPv4 address of the first worker node."
+  default     = "192.168.80.80/28"
+}
+
+variable "node_count" {
+  type        = number
+  description = "The number of worker nodes to create."
+  default     = 2
+}
