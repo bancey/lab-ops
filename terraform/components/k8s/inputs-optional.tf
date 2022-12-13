3e0a452e2be32f05baa2d2746c1ff4c2309c6f16
Alex Bance <alex.bance@live.co.uk>
2022-12-13 10:19:22 +0000

    fix: tweak ranges

--- terraform/components/k8s/inputs-optional.tf
+++ terraform/components/k8s/inputs-optional.tf
@@ -1,7 +1,7 @@
 variable "master_cidr" {
   type        = string
   description = "The IPv4 address of the master node."
-  default     = "192.168.80.64/28"
+  default     = "192.168.80.64/29"
 }
 
 variable "master_count" {
@@ -13,7 +13,7 @@ variable "master_count" {
 variable "nodes_cidr" {
   type        = string
   description = "The IPv4 address of the first worker node."
-  default     = "192.168.80.80/28"
+  default     = "192.168.80.72/29"
 }
 
 variable "node_count" {
