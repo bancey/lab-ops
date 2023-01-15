15ca3209ac66a796e64d346147e492301c12b090
Alex Bance <abance@bancey.xyz>
2023-01-15 10:54:43 +0000

    feat: added cname for wings (#37)

--- terraform/components/pteronode/interpolated-defaults.tf
+++ terraform/components/pteronode/interpolated-defaults.tf
@@ -19,3 +19,23 @@ data "azurerm_key_vault_secret" "proxmox_token_secret" {
   name         = "Wanda-Proxmox-Token-Secret"
   key_vault_id = data.azurerm_key_vault.vault.id
 }
+
+data "azurerm_key_vault_secret" "cloudflare_lab_zone_id" {
+  name         = "Cloudflare-Lab-Zone-ID"
+  key_vault_id = data.azurerm_key_vault.vault.id
+}
+
+data "azurerm_key_vault_secret" "cloudflare_lab_zone_name" {
+  name         = "Cloudflare-Lab-Zone-Name"
+  key_vault_id = data.azurerm_key_vault.vault.id
+}
+
+data "azurerm_key_vault_secret" "cloudflare_lab_api_token" {
+  name         = "Cloudflare-Lab-API-Token"
+  key_vault_id = data.azurerm_key_vault.vault.id
+}
+
+data "azurerm_key_vault_secret" "public_ip" {
+  name         = "Home-Public-IP"
+  key_vault_id = data.azurerm_key_vault.vault.id
+}
