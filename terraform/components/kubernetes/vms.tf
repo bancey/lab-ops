141a8c48d7ee05b8e880216ef863237c5a024e57
Alex Bance <alex.bance@live.co.uk>
2023-01-24 16:44:44 +0000

    fix: cleaned up tf

--- terraform/components/kubernetes/vms.tf
+++ terraform/components/kubernetes/vms.tf
@@ -1,44 +1,14 @@
-module "master" {
-  count  = var.master_count
-  source = "../../modules/proxmox-vm"
-
-  target_node         = "wanda"
-  vm_name             = "master${count.index}"
-  vm_id               = local.master_start_vm_id + count.index
-  vm_description      = "k8s control plane"
-  cpu_cores           = 2
-  memory              = 4096
-  ip_address          = cidrhost(var.master_cidr, count.index)
-  gateway_ip_address  = var.master_gateway_ip_address
-  network_bridge_name = "vmbr1"
-}
-
-module "nodes" {
-  count  = var.node_count
-  source = "../../modules/proxmox-vm"
-
-  target_node         = "wanda"
-  vm_name             = "node${count.index}"
-  vm_id               = local.node_start_vm_id + count.index
-  vm_description      = "k8s worker node"
-  cpu_cores           = 2
-  memory              = 4096
-  ip_address          = cidrhost(var.node_cidr, count.index)
-  gateway_ip_address  = var.node_gateway_ip_address
-  network_bridge_name = "vmbr1"
-}
-
 module "k3s_vm" {
   count  = local.vm_count
   source = "../../modules/proxmox-vm"
 
   target_node         = "wanda"
   vm_name             = count.index >= var.master_count ? "node${count.index - var.node_count}" : "master${count.index}"
-  vm_id               = local.master_start_vm_id + count.index
-  vm_description      = "k8s control plane"
+  vm_id               = count.index >= var.master_count ? local.node_start_vm_id + (count.index - var.node_count) : local.master_start_vm_id + count.index
+  vm_description      = count.index >= var.master_count ? "k8s worker node" : "k8s control plane"
   cpu_cores           = 2
   memory              = 4096
-  ip_address          = cidrhost(var.master_cidr, count.index)
-  gateway_ip_address  = var.master_gateway_ip_address
+  ip_address          = count.index >= var.master_count ? cidrhost(var.node_cidr, (count.index - var.node_count)) : cidrhost(var.master_cidr, count.index)
+  gateway_ip_address  = count.index >= var.master_count ? var.node_gateway_ip_address : var.master_gateway_ip_address
   network_bridge_name = "vmbr1"
 }
