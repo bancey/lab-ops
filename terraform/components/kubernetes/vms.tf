a414d4f916de18474196c512130d1cc29efd46b9
Alex Bance <alex.bance@live.co.uk>
2023-03-14 22:21:09 +0000

    fix: naming/id issues

--- terraform/components/kubernetes/vms.tf
+++ terraform/components/kubernetes/vms.tf
@@ -3,12 +3,12 @@ module "k3s_vm" {
   source = "../../modules/proxmox-vm"
 
   target_node         = "wanda"
-  vm_name             = count.index >= var.master_count ? "node${count.index - var.node_count -1}" : "master${count.index}"
-  vm_id               = count.index >= var.master_count ? local.node_start_vm_id + (count.index - var.node_count -1) : local.master_start_vm_id + count.index
+  vm_name             = count.index >= var.master_count ? "node${count.index - var.master_count -1}" : "master${count.index}"
+  vm_id               = count.index >= var.master_count ? local.node_start_vm_id + (count.index - var.master_count -1) : local.master_start_vm_id + count.index
   vm_description      = count.index >= var.master_count ? "k8s worker node" : "k8s control plane"
   cpu_cores           = 4
   memory              = 8096
-  ip_address          = count.index >= var.master_count ? cidrhost(var.node_cidr, (count.index - var.node_count -1)) : cidrhost(var.master_cidr, count.index)
+  ip_address          = count.index >= var.master_count ? cidrhost(var.node_cidr, (count.index - var.master_count -1)) : cidrhost(var.master_cidr, count.index)
   gateway_ip_address  = count.index >= var.master_count ? var.node_gateway_ip_address : var.master_gateway_ip_address
   network_bridge_name = var.network_bridge_name
   vlan_tag            = var.vlan_tag
