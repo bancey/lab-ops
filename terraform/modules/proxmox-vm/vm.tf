8d25b228b6f2da7f992abef92cccd101f2d55b70
Alex Bance <abance@bancey.xyz>
2023-06-03 15:26:02 +0100

    fix: added qemu_os property, added test vm

--- terraform/modules/proxmox-vm/vm.tf
+++ terraform/modules/proxmox-vm/vm.tf
@@ -14,6 +14,7 @@ resource "proxmox_vm_qemu" "vm" {
   os_type     = "cloud-init"
   ipconfig0   = "ip=${var.ip_address}/24,gw=${var.gateway_ip_address}"
   pool        = var.resource_pool
+  qemo_os     = "l26"
 
   network {
     bridge = var.network_bridge_name
