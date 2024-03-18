988083867d0e690692782cff50a9733dc1496b6f
Alex Bance <abance@bancey.xyz>
2024-03-18 08:18:22 +0000

    fix: update reference to pip

--- terraform/components/gameserver/dns-a.tf
+++ terraform/components/gameserver/dns-a.tf
@@ -2,7 +2,7 @@ resource "cloudflare_record" "records" {
   for_each = var.gameservers
   zone_id  = data.azurerm_key_vault_secret.cloudflare_main_zone_id.value
   name     = "${each.key}-${var.env}"
-  value    = azurerm_public_ip.this[count.index].ip_address
+  value    = azurerm_public_ip.this[each.key].ip_address
   type     = "A"
   proxied  = false
   ttl      = 100
