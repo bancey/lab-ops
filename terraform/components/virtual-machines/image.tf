c3e9061db09773ed458f375dab4e2c4f3106cccb
Alex Bance <abance@bancey.xyz>
2024-03-26 20:00:37 +0000

    fix: add separate images per node

--- terraform/components/virtual-machines/image.tf
+++ terraform/components/virtual-machines/image.tf
@@ -1,6 +1,31 @@
-resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+resource "proxmox_virtual_environment_download_file" "wanda_ubuntu_cloud_image" {
+  provider     = proxmox.wanda
   content_type = "iso"
   datastore_id = "local"
-  node_name    = var.target_node
+  node_name    = "wanda"
+  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+}
+
+resource "proxmox_virtual_environment_download_file" "hela_ubuntu_cloud_image" {
+  provider     = proxmox.hela
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "hela"
+  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+}
+
+resource "proxmox_virtual_environment_download_file" "thor_ubuntu_cloud_image" {
+  provider     = proxmox.thor
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "thor"
+  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+}
+
+resource "proxmox_virtual_environment_download_file" "loki_ubuntu_cloud_image" {
+  provider     = proxmox.loki
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "loki"
   url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
 }
