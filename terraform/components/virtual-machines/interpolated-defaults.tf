fd46bb06143a7ca1f941730b2e4628ddeba37e93
Alex Bance <abance@bancey.xyz>
2024-03-27 17:36:22 +0000

    fix: update vm names

--- terraform/components/virtual-machines/interpolated-defaults.tf
+++ terraform/components/virtual-machines/interpolated-defaults.tf
@@ -3,7 +3,7 @@ locals {
     for vm_key, vm_value in var.kubernetes_virtual_machines : [
       for i in range(vm_value.master.count) : {
         target_node         = length(vm_value.target_nodes) > 1 ? vm_value.target_nodes[i % length(vm_value.target_nodes)] : vm_value.target_nodes[0]
-        vm_name             = "master${i}"
+        vm_name             = "${vm_key}-master${i}"
         vm_id               = vm_value.master.vm_id_start + i
         vm_description      = "k8s control plane"
         cpu_cores           = 4
@@ -22,7 +22,7 @@ locals {
     for vm_key, vm_value in var.kubernetes_virtual_machines : [
       for i in range(vm_value.worker.count) : {
         target_node         = length(vm_value.target_nodes) > 1 ? vm_value.target_nodes[i % length(vm_value.target_nodes)] : vm_value.target_nodes[0]
-        vm_name             = "node${i}"
+        vm_name             = "${vm_key}-node${i}"
         vm_id               = vm_value.worker.vm_id_start + i
         vm_description      = "k8s worker node"
         cpu_cores           = 2
