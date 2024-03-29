319ff3b1fbd7cdb7da025f159bd37dfd881f2abf
Alex Bance <abance@bancey.xyz>
2024-03-29 21:22:19 +0000

    fix: simplify commands

--- terraform/components/virtual-machines/k8s-vms-config.tf
+++ terraform/components/virtual-machines/k8s-vms-config.tf
@@ -7,10 +7,7 @@ resource "terraform_data" "k8s_hosts" {
   for_each = { for value in concat(local.master_vms, local.worker_vms) : "${value.target_node}-${value.vm_name}" => value if contains(var.target_nodes, lower(value.target_node)) }
 
   provisioner "local-exec" {
-    command = <<EOF
-      ls
-      yq -i '.all.children.wanda_k3s_cluster.hosts.${each.value.vm_name}.ansible_host = "${each.value.ip_address}"' hosts.yaml
-    EOF
+    command     = "yq -i '.all.children.wanda_k3s_cluster.hosts.${each.value.vm_name}.ansible_host = \"${each.value.ip_address}\"' hosts.yaml"
     interpreter = ["/bin/bash", "-c"]
     working_dir = replace(path.cwd, "/terraform/components/virtual-machines", "/ansible")
   }
