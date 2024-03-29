06818a56ac4d5afe6f6d91dd7cc8c3d7d0196105
Alex Bance <abance@bancey.xyz>
2024-03-29 19:08:50 +0000

    feat: dynamically populate ansible inventory (#121)
    * feat: start rework of ansible
    
    * wip: update hosts from tf
    
    * feat: dynamically populate ansible inventory

---
+++ terraform/components/virtual-machines/k8s-vms-config.tf
@@ -0,0 +1,14 @@
+resource "terraform_data" "k8s_hosts" {
+  triggers_replace = [
+    local.master_vms,
+    local.worker_vms
+  ]
+
+  for_each = { for value in concat(local.master_vms, local.worker_vms) : "${value.target_node}-${value.vm_name}" => value if contains(var.target_nodes, lower(value.target_node)) }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      yq -i e 'all.children.wanda_k3s_cluster.hosts.${each.value.vm_name}.ansible_host = ${each.value.ip_address}' ${path.cwd}/../../ansible/hosts.yaml
+    EOF
+  }
+}
