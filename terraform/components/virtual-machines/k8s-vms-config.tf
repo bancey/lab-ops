789b8a4fa3b407971237b48e0720005129146bac
Alex Bance <abance@bancey.xyz>
2024-03-31 15:23:50 +0100

    fix: switch to script file

--- terraform/components/virtual-machines/k8s-vms-config.tf
+++ terraform/components/virtual-machines/k8s-vms-config.tf
@@ -7,15 +7,10 @@ resource "local_sensitive_file" "key_file" {
 resource "terraform_data" "k8s_ansible" {
   for_each = { for key, value in var.kubernetes_virtual_machines : key => value if var.target_nodes == value.target_nodes }
 
-  depends_on = [ local_sensitive_file.key_file ]
+  depends_on = [local_sensitive_file.key_file]
 
   provisioner "local-exec" {
-    command     = <<EOT
-      eval `ssh-agent -s`
-      ssh-add id_rsa
-      ansible-galaxy install -r requirements.yml
-      ansible-playbook --inventory hosts.yaml k3s.yaml --extra-vars run_prep=true run_install=true passed_hosts=${each.key}_k3s_cluster
-    EOT
+    command     = file("${path.module}/ansible.sh.tpl")
     working_dir = replace(path.cwd, "/terraform/components/virtual-machines", "/ansible")
     interpreter = ["/bin/bash", "-c"]
   }
