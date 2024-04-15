e2eda41a91eb562b25f937fdcb5e8f011e816218
Alex Bance <abance@bancey.xyz>
2024-04-15 15:24:13 +0100

    fix: update dependsOn

--- terraform/components/virtual-machines/k8s-vms-config.tf
+++ terraform/components/virtual-machines/k8s-vms-config.tf
@@ -8,7 +8,13 @@ resource "terraform_data" "k8s_ansible" {
   triggers_replace = timestamp()
   for_each         = { for key, value in var.kubernetes_virtual_machines : key => value if var.target_nodes == value.target_nodes }
 
-  depends_on = [local_sensitive_file.key_file]
+  depends_on = [
+    local_sensitive_file.key_file,
+    module.wanda_k8s_virtual_machines,
+    module.hela_k8s_virtual_machines,
+    module.thor_k8s_virtual_machines,
+    module.loki_k8s_virtual_machines
+  ]
 
   provisioner "local-exec" {
     command = templatefile("${path.module}/ansible.sh.tpl", {
