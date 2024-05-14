263b969ef876c7ab7a66cc5be01a290524ac0ef4
Alex Bance <abance@bancey.xyz>
2024-05-14 17:12:02 +0100

    fix: tweak provision script

--- terraform/components/gameserver/provision/setup-twingate.sh
+++ terraform/components/gameserver/provision/setup-twingate.sh
@@ -1,4 +1,5 @@
-echo "${TWINGATE_SERVICE_KEY}" > servicekey.json
+sudo mkdir -p /opt/twingate
+echo "${TWINGATE_SERVICE_KEY}" > /opt/twingate/servicekey.json
 curl https://binaries.twingate.com/client/linux/install.sh | sudo bash
-sudo twingate setup --headless servicekey.json
+sudo twingate setup --headless /opt/twingate/servicekey.json
 sudo twingate start
