dcbd70df111be3700a162acd25a9f29511c0a5db
Alex Bance <abance@bancey.xyz>
2024-05-14 17:23:30 +0100

    fix: use single over double quotes

--- terraform/components/gameserver/provision/setup-twingate.sh
+++ terraform/components/gameserver/provision/setup-twingate.sh
@@ -1,5 +1,5 @@
 sudo mkdir -p /opt/twingate
-echo "${TWINGATE_SERVICE_KEY}" > /opt/twingate/servicekey.json
+echo '${TWINGATE_SERVICE_KEY}' > /opt/twingate/servicekey.json
 curl https://binaries.twingate.com/client/linux/install.sh | sudo bash
 sudo twingate setup --headless /opt/twingate/servicekey.json
 sudo twingate start
