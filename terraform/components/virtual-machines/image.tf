777d574bd9eae102077675ba96a159ad4482f8be
Alex Bance <abance@bancey.xyz>
2024-06-10 11:35:09 +0100

    fix: correct urls

--- terraform/components/virtual-machines/image.tf
+++ terraform/components/virtual-machines/image.tf
@@ -4,7 +4,7 @@ resource "proxmox_virtual_environment_download_file" "wanda_ubuntu_cloud_image" 
   content_type = "iso"
   datastore_id = "local"
   node_name    = "wanda"
-  url          = "https://cloud-images.ubuntu.com/jammy/20240416/jammy-server-cloudimg-amd64.img"
+  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
 }
 
 resource "proxmox_virtual_environment_download_file" "hela_ubuntu_cloud_image" {
@@ -13,7 +13,7 @@ resource "proxmox_virtual_environment_download_file" "hela_ubuntu_cloud_image" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "hela"
-  url          = "https://cloud-images.ubuntu.com/jammy/20240416/jammy-server-cloudimg-amd64.img"
+  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
 }
 
 resource "proxmox_virtual_environment_download_file" "thor_ubuntu_cloud_image" {
@@ -22,7 +22,7 @@ resource "proxmox_virtual_environment_download_file" "thor_ubuntu_cloud_image" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "thor"
-  url          = "https://cloud-images.ubuntu.com/jammy/20240416/jammy-server-cloudimg-amd64.img"
+  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
 }
 
 resource "proxmox_virtual_environment_download_file" "loki_ubuntu_cloud_image" {
@@ -31,5 +31,5 @@ resource "proxmox_virtual_environment_download_file" "loki_ubuntu_cloud_image" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "loki"
-  url          = "https://cloud-images.ubuntu.com/jammy/20240416/jammy-server-cloudimg-amd64.img"
+  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
 }
