f9ee27853069cdd9dc2775d96367ef35cce3090c
Alex Bance <abance@bancey.xyz>
2024-06-11 09:42:24 +0100

    fix: each.value -> count.index

--- terraform/components/virtual-machines/image.tf
+++ terraform/components/virtual-machines/image.tf
@@ -4,8 +4,8 @@ resource "proxmox_virtual_environment_download_file" "wanda_ubuntu_cloud_image" 
   content_type = "iso"
   datastore_id = "local"
   node_name    = "wanda"
-  file_name    = "${each.value.ubuntu_version}-server-cloudimg-amd64-${each.value.ubuntu_image_version}.img"
-  url          = "https://cloud-images.ubuntu.com/${each.value.ubuntu_version}/${each.value.ubuntu_image_version}/jammy-server-cloudimg-amd64.img"
+  file_name    = "${var.ubuntu_images[count.index].ubuntu_version}-server-cloudimg-amd64-${var.ubuntu_images[count.index].ubuntu_image_version}.img"
+  url          = "https://cloud-images.ubuntu.com/${var.ubuntu_images[count.index].ubuntu_version}/${var.ubuntu_images[count.index].ubuntu_image_version}/jammy-server-cloudimg-amd64.img"
 }
 
 resource "proxmox_virtual_environment_download_file" "hela_ubuntu_cloud_image" {
@@ -14,8 +14,8 @@ resource "proxmox_virtual_environment_download_file" "hela_ubuntu_cloud_image" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "hela"
-  file_name    = "${each.value.ubuntu_version}-server-cloudimg-amd64-${each.value.ubuntu_image_version}.img"
-  url          = "https://cloud-images.ubuntu.com/${each.value.ubuntu_version}/${each.value.ubuntu_image_version}/jammy-server-cloudimg-amd64.img"
+  file_name    = "${var.ubuntu_images[count.index].ubuntu_version}-server-cloudimg-amd64-${var.ubuntu_images[count.index].ubuntu_image_version}.img"
+  url          = "https://cloud-images.ubuntu.com/${var.ubuntu_images[count.index].ubuntu_version}/${var.ubuntu_images[count.index].ubuntu_image_version}/jammy-server-cloudimg-amd64.img"
 }
 
 resource "proxmox_virtual_environment_download_file" "thor_ubuntu_cloud_image" {
@@ -24,8 +24,8 @@ resource "proxmox_virtual_environment_download_file" "thor_ubuntu_cloud_image" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "thor"
-  file_name    = "${each.value.ubuntu_version}-server-cloudimg-amd64-${each.value.ubuntu_image_version}.img"
-  url          = "https://cloud-images.ubuntu.com/${each.value.ubuntu_version}/${each.value.ubuntu_image_version}/jammy-server-cloudimg-amd64.img"
+  file_name    = "${var.ubuntu_images[count.index].ubuntu_version}-server-cloudimg-amd64-${var.ubuntu_images[count.index].ubuntu_image_version}.img"
+  url          = "https://cloud-images.ubuntu.com/${var.ubuntu_images[count.index].ubuntu_version}/${var.ubuntu_images[count.index].ubuntu_image_version}/jammy-server-cloudimg-amd64.img"
 }
 
 resource "proxmox_virtual_environment_download_file" "loki_ubuntu_cloud_image" {
@@ -34,6 +34,6 @@ resource "proxmox_virtual_environment_download_file" "loki_ubuntu_cloud_image" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "loki"
-  file_name    = "${each.value.ubuntu_version}-server-cloudimg-amd64-${each.value.ubuntu_image_version}.img"
-  url          = "https://cloud-images.ubuntu.com/${each.value.ubuntu_version}/${each.value.ubuntu_image_version}/jammy-server-cloudimg-amd64.img"
+  file_name    = "${var.ubuntu_images[count.index].ubuntu_version}-server-cloudimg-amd64-${var.ubuntu_images[count.index].ubuntu_image_version}.img"
+  url          = "https://cloud-images.ubuntu.com/${var.ubuntu_images[count.index].ubuntu_version}/${var.ubuntu_images[count.index].ubuntu_image_version}/jammy-server-cloudimg-amd64.img"
 }
