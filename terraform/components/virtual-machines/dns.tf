38f8ee19b6d31b52e556a28214d1bbeceb3a6382
Alex Bance <abance@bancey.xyz>
2024-09-21 20:59:22 +0100

    fix: switch from value to content (#305)
    * fix: switch from value to content
    
    * fix: fmt

--- terraform/components/virtual-machines/dns.tf
+++ terraform/components/virtual-machines/dns.tf
@@ -6,7 +6,7 @@ resource "cloudflare_record" "wings_cname" {
 
   zone_id = data.azurerm_key_vault_secret.cloudflare_lab_zone_id.value
   name    = each.key
-  value   = data.azurerm_key_vault_secret.cloudflare_lab_zone_name.value
+  content = data.azurerm_key_vault_secret.cloudflare_lab_zone_name.value
   type    = "CNAME"
   proxied = "true"
   ttl     = 1
