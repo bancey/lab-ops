90338501a994ee9d7282e86f2cbd78d69625f767
Alex Bance <abance@bancey.xyz>
2024-09-22 13:01:00 +0100

    fix: remove debug

--- terraform/components/virtual-machines/vms-lxc-config.tf
+++ terraform/components/virtual-machines/vms-lxc-config.tf
@@ -16,11 +16,7 @@ resource "terraform_data" "ansible" {
 
   triggers_replace = [
     each.value.trigger,
-    file("../../../ansible/${each.value.playbook}"),
-    nonsensitive(templatefile("${path.module}/ansible.sh.tpl", {
-      playbook  = each.value.playbook
-      arguments = "${join(" ", flatten([for arg_name, secret_name in each.value.secrets : [ "-e ${arg_name}=${data.azurerm_key_vault_secret.ansible_secrets["${each.key}_${arg_name}"].value}" ] if length(each.value.secrets) > 0]))}${each.value.arguments}"
-    }))
+    file("../../../ansible/${each.value.playbook}")
   ]
 
   provisioner "local-exec" {
