27d6acb194194cef1c5ee90e9da3c68a96eda093
Alex Bance <abance@bancey.xyz>
2024-12-19 13:46:33 +0000

    fix: correct scenario when a vpn gateway is inactive

--- terraform/components/cloud-vpn-gateway/interpolated-defaults.tf
+++ terraform/components/cloud-vpn-gateway/interpolated-defaults.tf
@@ -1,6 +1,6 @@
 locals {
   name                = "${var.cloud_vpn_gateway.name}-${var.env}"
-  resource_group_name = var.cloud_vpn_gateway.existing_resource_group_name != null ? var.cloud_vpn_gateway.existing_resource_group_name : azurerm_resource_group.this[0].name
+  resource_group_name = var.cloud_vpn_gateway.existing_resource_group_name != null ? var.cloud_vpn_gateway.existing_resource_group_name : var.cloud_vpn_gateway.active ? azurerm_resource_group.this[0].name : null
   tags = merge(
     var.cloud_vpn_gateway.tags,
     {
