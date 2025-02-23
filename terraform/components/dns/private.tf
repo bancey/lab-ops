9d490ce4ac2677f2fd4806b22ae9f7993cb07b2d
Alex Bance <abance@bancey.xyz>
2025-02-23 19:39:45 +0000

    fix: use removed block

--- terraform/components/dns/private.tf
+++ terraform/components/dns/private.tf
@@ -15,19 +15,26 @@ module "adguard_thanos" {
   }
 }
 
+module "adguard_gamora" {
+  source = "../../modules/adguard"
+  providers = {
+    adguard = adguard.gamora
+  }
+
+  adguard_rewrites   = local.adguard_rewrites
+  adguard_filters    = var.adguard_filters
+  adguard_user_rules = var.adguard_user_rules
+  adguard_config = {
+    dns = {
+      upstream_dns = ["https://dns10.quad9.net/dns-query", "[/*.home.bancey.xyz/]10.151.14.1"]
+    }
+  }
+}
+
 # My Raspberry Pi died
-# module "adguard_gamora" {
-#   source = "../../modules/adguard"
-#   providers = {
-#     adguard = adguard.gamora
-#   }
-# 
-#   adguard_rewrites   = local.adguard_rewrites
-#   adguard_filters    = var.adguard_filters
-#   adguard_user_rules = var.adguard_user_rules
-#   adguard_config = {
-#     dns = {
-#       upstream_dns = ["https://dns10.quad9.net/dns-query", "[/*.home.bancey.xyz/]10.151.14.1"]
-#     }
-#   }
-# }
+removed {
+  from = module.adguard_gamora
+  lifecycle {
+    destroy = false
+  }
+}
