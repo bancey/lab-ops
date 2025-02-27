c16dda8e6c3fa1573b387e16cd9531685485eeaa
Alex Bance <abance@bancey.xyz>
2025-02-27 09:22:34 +0000

    fix: re-enable adguard config

--- terraform/components/dns/private.tf
+++ terraform/components/dns/private.tf
@@ -15,25 +15,18 @@ module "adguard_thanos" {
   }
 }
 
-# My Raspberry Pi died
-# module "adguard_gamora" {
-#   source = "../../modules/adguard"
-#   providers = {
-#     adguard = adguard.gamora
-#   }
-# 
-#   adguard_rewrites   = local.adguard_rewrites
-#   adguard_filters    = var.adguard_filters
-#   adguard_user_rules = var.adguard_user_rules
-#   adguard_config = {
-#     dns = {
-#       upstream_dns = ["https://dns10.quad9.net/dns-query", "[/*.home.bancey.xyz/]10.151.14.1"]
-#     }
-#   }
-# }
-removed {
-  from = module.adguard_gamora
-  lifecycle {
-    destroy = false
+module "adguard_gamora" {
+  source = "../../modules/adguard"
+  providers = {
+    adguard = adguard.gamora
+  }
+
+  adguard_rewrites   = local.adguard_rewrites
+  adguard_filters    = var.adguard_filters
+  adguard_user_rules = var.adguard_user_rules
+  adguard_config = {
+    dns = {
+      upstream_dns = ["https://dns10.quad9.net/dns-query", "[/*.home.bancey.xyz/]10.151.14.1"]
+    }
   }
 }
