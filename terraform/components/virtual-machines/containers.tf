e3b833b28c9a3f0528e33d6bfee21a1d7d7c0d04
Alex Bance <abance@bancey.xyz>
2026-04-21 20:23:58 +0100

    feat: extend lxc disk size (#1167)
    * feat: extend lxc disk size
    
    * fix: dont bump pgsql and fix default
    
    * fix: correct issue with twingate alias

--- terraform/components/virtual-machines/containers.tf
+++ terraform/components/virtual-machines/containers.tf
@@ -20,6 +20,7 @@ module "wanda_containers" {
   cpu_cores           = each.value.cpu_cores
   memory              = each.value.memory
   unprivileged        = each.value.unprivileged
+  disk_size           = each.value.disk_size
 
   image_id   = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
   image_type = "ubuntu"
@@ -46,6 +47,7 @@ module "tiny_containers" {
   startup_delay       = each.value.startup_delay
   cpu_cores           = each.value.cpu_cores
   memory              = each.value.memory
+  disk_size           = each.value.disk_size
 
   image_id   = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
   image_type = "ubuntu"
