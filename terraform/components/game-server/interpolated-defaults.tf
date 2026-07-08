22960e0cf20ffc335ad966d9be34561174128942
Alex Bance <abance@bancey.xyz>
2026-07-08 10:24:23 +0100

    fix: remove remanents

--- terraform/components/game-server/interpolated-defaults.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_key_vault" "vault" {
-  name                = "bancey-vault"
-  resource_group_name = "common"
-}
-
-data "azurerm_key_vault_secret" "cloudflare_main_api_token" {
-  name         = "Cloudflare-Main-API-Token"
-  key_vault_id = data.azurerm_key_vault.vault.id
-}
