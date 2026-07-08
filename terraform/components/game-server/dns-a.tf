7994ce8faaa7a8f2472c6744121e529a76d0056b
Alex Bance <abance@bancey.xyz>
2026-07-08 09:44:00 +0100

    feat: remove game servers (#1379)
    * feat: remove game servers
    
    * fix: set destroy: true
    
    * fix: remove vars
    
    * fix: re-add data block so destroy can take place

--- terraform/components/game-server/dns-a.tf
+++
@@ -1,9 +0,0 @@
-resource "cloudflare_dns_record" "records" {
-  for_each = var.game_servers
-  zone_id  = data.azurerm_key_vault_secret.cloudflare_main_zone_id.value
-  name     = "${each.key}-${var.env}"
-  content  = azurerm_public_ip.this[each.key].ip_address
-  type     = "A"
-  proxied  = false
-  ttl      = 100
-}
--- terraform/components/game-server/interpolated-defaults.tf
+++ terraform/components/game-server/interpolated-defaults.tf
@@ -1,60 +1,9 @@
-data "azurerm_client_config" "current" {}
-
-locals {
-  tags = {
-    "application" = "game_servers",
-    "environment" = var.env,
-  }
-
-  private_nsg_rules = {
-    for k, v in module.private_nsg_rules.nsg_rules : k => merge(v, { type = "private" })
-  }
-  public_nsg_rules = {
-    for k, v in module.public_nsg_rules.nsg_rules : k => merge(v, { type = "public" })
-  }
-}
-
-module "private_nsg_rules" {
-  source = "./game-server-nsg"
-}
-
-module "public_nsg_rules" {
-  source                = "./game-server-nsg"
-  source_address_prefix = data.azurerm_key_vault_secret.public_ip.value
-  publicly_accessible   = true
-}
-
 data "azurerm_key_vault" "vault" {
   name                = "bancey-vault"
   resource_group_name = "common"
 }
 
-data "azurerm_key_vault_secret" "cloudflare_main_zone_id" {
-  name         = "Cloudflare-Main-Zone-ID"
-  key_vault_id = data.azurerm_key_vault.vault.id
-}
-
-data "azurerm_key_vault_secret" "cloudflare_main_zone_name" {
-  name         = "Cloudflare-Main-Zone-Name"
-  key_vault_id = data.azurerm_key_vault.vault.id
-}
-
 data "azurerm_key_vault_secret" "cloudflare_main_api_token" {
   name         = "Cloudflare-Main-API-Token"
-  key_vault_id = data.azurerm_key_vault.vault.id
-}
-
-data "azurerm_key_vault_secret" "game_server_ports" {
-  name         = "Game-Server-Ports"
-  key_vault_id = data.azurerm_key_vault.vault.id
-}
-
-data "azurerm_key_vault_secret" "public_ip" {
-  name         = "Home-Public-IP"
-  key_vault_id = data.azurerm_key_vault.vault.id
-}
-
-data "azurerm_key_vault_secret" "twingate_pelican_sa_key" {
-  name         = "Twingate-Pelican-SA-Key"
   key_vault_id = data.azurerm_key_vault.vault.id
 }
