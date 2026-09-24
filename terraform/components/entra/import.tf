a82f20f1f8bac349be169af738683b47f0715319
Alex Bance <abance@bancey.xyz>
2026-09-24 16:36:02 +0100

    feat: import existing app reg/sp

---
+++ terraform/components/entra/import.tf
@@ -0,0 +1,9 @@
+import {
+    to = azuread_application.this["lab-kubernetes"]
+    id = "/applications/666ef8d0-700a-417b-967b-ec81e2742c75"
+}
+
+import {
+    to = azuread_service_principal.this["lab-kubernetes"]
+    id = "666ef8d0-700a-417b-967b-ec81e2742c75"
+}
